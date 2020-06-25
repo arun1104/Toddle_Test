bec7e0d37d1b8413ebd79dab971668fc76b9ced3
unknown <arunrajeevan1104@gmail.com>
2020-06-25 21:13:59 +0530

    added middleware changes for swagger viewer

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,17 +6,22 @@ const filePath = path.join(__dirname, 'private');
 const jwt = require('jsonwebtoken');
 module.exports.authMiddleware = function(req, res, next) {
   try {
-    let operationId = req.swagger.operation.operationId.toLowerCase();
-    let publicApis = ['signup', 'login'];
-    if (!publicApis.includes(operationId)){
-      var cert = fs.readFileSync(filePath);
-      var decoded = jwt.verify(req.headers.authorization, cert, { algorithms: ['RS256'] });
-      req.body.createdBy = decoded.userId;
-      console.log(decoded);
-      next();
+    if (req.swagger){
+      let operationId = req.swagger.operation.operationId.toLowerCase();
+      let publicApis = ['signup', 'login'];
+      if (!publicApis.includes(operationId)){
+        var cert = fs.readFileSync(filePath);
+        var decoded = jwt.verify(req.headers.authorization, cert, { algorithms: ['RS256'] });
+        req.body.createdBy = decoded.userId;
+        console.log(decoded);
+        next();
+      } else {
+        next();
+      }
     } else {
       next();
     }
+
   } catch (err) {
     res.status(400).send({message: 'Invalid token'});
   }
